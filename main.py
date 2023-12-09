00d3f6546be61e338aa97dee9e0fe000c0b82e49
SamAkinbile <>
2023-12-09 16:47:44 +0000

    select word added

--- main.py
+++ main.py
@@ -1,5 +1,1 @@
-import random
-print("hello is the person")
-name = input("wat is your name?")
-my_name_is = ["dapo" "tolu" "heh,"]
-print("my_name_is")
+select_word = ["aardvark", "baboon", "camel"]

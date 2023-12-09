be9e62688aa68b45a70faa4c6c3bdd32f0ac1ecc
SamAkinbile <>
2023-12-09 18:54:02 +0000

    main.py updated

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import random
 
-
 select_list = ["aardvark", "baboon", "camel"]
 myword = random.choice(select_list)
 
@@ -10,19 +9,24 @@ word_length = len(myword)
 for _ in range(word_length):
     appear_as += "_"
     print(appear_as)
+
+game_finish = False
 while not game_finish:
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input("Guess a letter: ").lower()
+    # TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+    guess = input("Guess a letter: ").lower()
 
+    # Check guessed letter
+    for position in range(word_length):
+        letter = myword[position]
 
-# Check guessed letter
-for position in range(word_length):
-    letter = myword[position]
-    print(f"Current position: {position}\n Current letter:{letter}\n Guessed letter: {guess}")
-    if letter == guess:
-        appear_as[position] = letter
+        print(
+            f"Current position: {position}\n    Current letter: {letter}\n    Guessed letter: {guess}")
 
-print(appear_as)
-if "_" not in display:
-    game_finish = True
-    print("You win.")
+        if letter == guess:
+            appear_as[position] = letter
+
+    print(appear_as)
+
+    if "_" not in appear_as:
+        game_finish = True
+        print("You win.")

644597358667bf04986b2b0d2e2a59800670b57e
SamAkinbile <>
2023-12-09 20:13:19 +0000

    logo imported

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from hangman_art import logo
 import random
 
 from hangman_guess_words import guess_word
@@ -5,3 +6,12 @@ from hangman_guess_words import guess_word
 
 myword = random.choice(guess_word)
 word_length = len(myword)
+
+
+end_of_game = False
+chances = 6
+
+# Import the logo from hangman.py and print it at the start of the game.
+
+from hangman import logo
+print(logo)

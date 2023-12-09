ea5bb9f28f108e8714f60ad17ac9ce684d432814
SamAkinbile <>
2023-12-09 20:34:53 +0000

    guessed pattern shift

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ myword = random.choice(guess_word)
 word_length = len(myword)
 
 
-end_of_game = False
+game_finish = False
 chances = 6
 
 # Logo imported from hangman files
@@ -18,4 +18,20 @@ print(logo)
 # Create blanks
 appear_as = []
 for _ in range(word_length):
-    display += "_"
+    appear_as += "_"
+
+while not game_finish:
+    trial_guess = input("Guess a letter:").lower()
+
+# If letter have already been guessed, print out the letter and let the player know
+
+if trial_guess in appear_as:
+    print(f"You have already guessed {trial_guess}")
+
+#To check the guessed letter
+
+for position in range(word_length):
+    letter = myword[position]
+
+    if letter == guess:
+        appear_as[position] = letter
